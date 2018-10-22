5d89c4b4205b3c0d24035278783757a9ff1a328a
jasehackman <jasehackman@gmail.com>
2018-10-22 13:11:09 -0500

    completed first two parts

--- main.js
+++ main.js
@@ -1,59 +1,64 @@
-// C is for Cookie
-const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
-let y = 1
+// // C is for Cookie
+// const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+// let y = 1
 
-for (let x = 1; y < cookies.length; x++) {
-    const currentCookie = cookies[y]
-    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
-}
+// for (let x = 0; x < cookies.length; x++) {
+//     const currentCookie = cookies[x]
+//     console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+// }
 
 // Conjunction Function
-const conjunction = function (firstWord, secondWord) {
-    const conjoinedWord = `${firstWord} ${secondWord}`
-}
+// let conjoinedWord = ""
+// const conjunction = function (firstWord, secondWord) {
+//     conjoinedWord = `${firstWord} ${secondWord}`
+// }
 
-conjunction("Master", "Card")
-console.log(conjoinedWord)
+// conjunction("Master", "Card")
+// console.log(conjoinedWord)
 
 // Mod Squad
+
+let HTMLRepresentation = `<h1>The Mod Squad</h1>`
 {
     const ModSquad = {
-        "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"]
+        "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
         "series": {
             "start": "1968",
             "end": "1973"
         }
     }
 
-    const HTMLRepresentation = `<h1>The Mod Squad</h1>`
+    
 
     ModSquad.members.forEach(member => {
-        const HTMLRepresentation += `<div>${member}</div>`
+        HTMLRepresentation += `<div>${member}</div>`
     })
+    console.log(HTMLRepresentation);
 }
+
 
 document.querySelector(".show-info").innerHTML = HTMLRepresentation
 
-// Simon Says
-// There are two separate scoping issues in this code. Remember, for and if are their own block scopes and if something is declared with let or const then that variable only exists in that scope.
+// // Simon Says
+// // There are two separate scoping issues in this code. Remember, for and if are their own block scopes and if something is declared with let or const then that variable only exists in that scope.
 
-const locations = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3]]
+// const locations = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3]]
 
-for (let k = 0; k < locations.length; k++) {
-    const currentLocation = locations[k]
+// for (let k = 0; k < locations.length; k++) {
+//     const currentLocation = locations[k]
 
-    if (currentLocation[0] > 2) {
-        const invalidLocation = true
-    }
+//     if (currentLocation[0] > 2) {
+//         const invalidLocation = true
+//     }
 
-    if (invalidLocation) {
-        console.log("This location is invalid")
-    }
-}
+//     if (invalidLocation) {
+//         console.log("This location is invalid")
+//     }
+// }
 
-/*
-    Since k is used as the loop counter, it will be the
-    total number of locations, not the invalid ones. You
-    need to fix this somehow.
-*/
-console.log(`There were ${k} invalid locations`)
+// /*
+//     Since k is used as the loop counter, it will be the
+//     total number of locations, not the invalid ones. You
+//     need to fix this somehow.
+// */
+// console.log(`There were ${k} invalid locations`)
